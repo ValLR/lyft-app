fa055df16d0c606b51fb475d9874944ab8db9b51
mily rivas <mrise.translations@gmail.com>
2017-07-03 11:30:02 -0400

    validar usuario e email listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
-
+	$(".submit-button").click(function(){
+		if(!(/^([a-zñáéíóú]{2,13})+$/.test($("#icon_name").val()))){
+   			$("#icon_name").append($("#icon_name").val("Error"));
+  		}
+  		if(!(/^([a-zñáéíóú]{2,13})+$/.test($("#icon_lastname").val()))){
+   			$("#icon_lastname").append($("#icon_lastname").val("Error"));
+  		}
+  		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#icon_email").val())) ){
+   			$("#icon_email").append($("#icon_email").val("Error"));
+  		}
+  	})
 });
-
