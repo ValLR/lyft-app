409d94bd8145f33e65aaa22dd4bc5dea874fd9cc
laura <laureen.pelaez.isaza@gmail.com>
2017-07-03 12:10:53 -0400

    updated links buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-	$(".submit-button").click(function(){
-		if(!(/^([a-zñáéíóú]{2,13})+$/.test($("#icon_name").val()))){
-   			$("#icon_name").append($("#icon_name").val("Error"));
-  		}
-  		if(!(/^([a-zñáéíóú]{2,13})+$/.test($("#icon_lastname").val()))){
-   			$("#icon_lastname").append($("#icon_lastname").val("Error"));
-  		}
-  		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#icon_email").val())) ){
-   			$("#icon_email").append($("#icon_email").val("Error"));
-  		}
-  	})
-});
-=======
+
   $(document).ready(function() {
     $('select').material_select();
   });
@@ -35,4 +21,4 @@ $("#next").click(function(){
   alert(" Tu código es LAB-" + aleatorio(100,999))
 });
 
->>>>>>> master
+
