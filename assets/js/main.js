f288888df8c14e155179812c48ff1c251dceede1
ValLR <valentinalopezrecabal@gmail.com>
2017-07-03 10:42:41 -0400

    estrutura html sin funcionalidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
-$(document).ready(function () {
+  $(document).ready(function() {
+    $('select').material_select();
+  });
 
+$("#telefono").on("keyup", function(){
+  $("#next").prop("disabled",false);
+  if($("#telefono").val() != 9){
+    $("#next").prop("disabled", true);
+  }
+})
+
+$("#next").click(function(){
+  alert("heyheyhey");
 });
-
