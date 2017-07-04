153e3dbbc8862287d47076f5cbb44be44b439ada
Gilia Gongora <giliagongora.g@gmail.com>
2017-07-03 20:12:22 -0400

    Solucionando detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
     $('select').material_select();
   });
+$(document).ready(function() {
+    $("#next").click(function(){
+ if($("#telefono").val().length != 9){
+   alert("Debes ingresar 9 números")
+}else{
+      $("#next").attr("href","input-code.html");
+     alert(" Tu código es LAB-" + aleatorio(100,999))
+}
+});
+
 
 
 function aleatorio(inferior,superior){
        var resAleatorio = Math.floor((Math.random() * (superior - inferior + 1)) + inferior);
       return resAleatorio;
 }
-
-
-
-$("#next").click(function(){
-  if($("#telefono").val().length === 9){  
-      $("#next").attr("href","input-code.html");
-     return alert(" Tu código es LAB-" + aleatorio(100,999))
-
- }
-  else{
-    alert("Please write a valid number")
-  }
-
-});
+  });
