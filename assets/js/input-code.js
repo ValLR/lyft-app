3944f686f2d78b1c6da4556fe6ac5d64efc54fc3
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-03 21:17:09 -0400

    sign-up functions working

--- assets/js/input-code.js
+++ assets/js/input-code.js
@@ -1,24 +1,25 @@
- $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
+$(document).ready(function () {
+  Materialize.updateTextFields();
+});
 
- /*$(document).ready(function () {
-	$("#sendbtn").click(function(){
-		if($(".imput-code").val()).length < 3  && $(".imput-code").val()).length > 4 ){
-  
-}
-  	})
-});*/
-       
-$("#sendbtn").click(function(){
+/*$(document).ready(function () {
+ $("#sendbtn").click(function(){
+   if($(".imput-code").val()).length < 3  && $(".imput-code").val()).length > 4 ){
  
-  if($("#first_name2").val().length == ""){  
+}
+   })
+});*/
+
+$("#sendbtn").click(function () {
+
+  if ($("#first_name2").val().length == "") {
     alert("Enter your code")
- } else if($("#first_name2").val().length < 7){
+  } else if ($("#first_name2").val().length < 7) {
     alert("I'm sorry, there are missing numbers")
- } else if($("#first_name2").val().length > 7){
+  } else if ($("#first_name2").val().length > 7) {
     alert("I'm sorry, there are too many numbers")
- } else if($("#first_name2").val().length === 7){
-    alert("Very well, enjoy your promotional code!")
- }
+  } else if ($("#first_name2").val().length === 7) {
+    alert("Very well, enjoy your promotional code!");
+    window.location.href = "sign-up-usuario.html";
+  }
 });
